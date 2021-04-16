cfe5eebf70a91c1f934e627b1cd2d9d0e8f979b3
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-04-16 12:49:50 +0200

    template add category form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,10 +16,10 @@ class CategoryController extends AbstractController
      */
     public function index(): Response
     {
-
         $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+        
 
-        if(!$categories) {
+        if (!$categories) {
             throw $this->createNotFoundException('No category found ! ');
         }
 
@@ -28,23 +29,36 @@ class CategoryController extends AbstractController
     }
 
     /**
-     * 
+     *
      * @Route("/category/{name}", name="category_show")
      */
     public function show(string $name): Response
-    {        
-        if(!$name) {
+    {
+        if (!$name) {
             throw $this->createNotFoundException('No category found ! ');
-        }else{
+        } else {
             $categoryName = $this->getDoctrine()->getRepository(Category::class)->findOneByName($name);
 
-            $categories = $this->getDoctrine()->getRepository(Program::class)->findBy(['category' => 1], ['id' => 'DESC'], 999);
-    
-           return $this->render('category/show.html.twig', [
+            $categories = $this->getDoctrine()->getRepository(Program::class)->findBy(['category' => $categoryName->getId()], ['id' => 'DESC'], 5);
+            
+            return $this->render('category/show.html.twig', [
                'categories' => $categories,
                'category_name' => $categoryName
            ]);
         }
+    }
 
+    /**
+     * @Route("/new", name="new_category")
+     */
+    public function new(): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView()
+        ]);
     }
 }
